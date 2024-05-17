298171d1f543b383b93e6f82a0aa03651d03fb1b
DESKTOP-UFEEQ7V\pasca <Pascal.keller12@gmail.com>
2024-05-17 16:15:28 +0200

    FinishedTimer

--- Assets/Scripts/Pascal/UITimer.cs
+++ Assets/Scripts/Pascal/UITimer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
@@ -6,29 +7,48 @@ using UnityEngine;
 
 public class UITimer : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro timerTextBox;
+    [SerializeField] private TextMeshProUGUI timerTextBox;
 
     [SerializeField] private float endTime;
 
     private bool paused;
 
-    private Timer timer;
+    private float timer;
     private void OnEnable()
     {
-        PauseMenu.TogglePause += OnPauseToggle();
+        PauseMenu.TogglePause += OnPauseToggle;
     }
+
+
     private void OnDisable()
     {
-        PauseMenu.TogglePause -= OnPauseToggle();
+        PauseMenu.TogglePause -= OnPauseToggle;
     }
     private void Start()
     {
-        timer.Start();
+        timer = 0f;
     }
 
     void Update()
     {
-        
+        var minutes = (int)(timer / 60);
+        var seconds = (int)(timer - minutes * 60);
+        Debug.Log(minutes.ToString() + ":" + seconds.ToString("00"));
+        Debug.Log(((int)minutes).ToString() + ":" + ((int)seconds).ToString("00"));
+        timerTextBox.text = ((int)minutes).ToString() + ":" + ((int)seconds).ToString("00");
+        if(timer >  endTime)
+        {
+            TimerIsUp();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void TimerIsUp()
+    {
+        SceneLoader.Instance.LoadScene(MyScenes.MainMenu);
     }
 
     private void OnPauseToggle(bool _paused)
